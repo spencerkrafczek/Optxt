3063501e1c8b547851fd150bcc908d509eae3ad2
Spencer Krafczek <spencerbdk@gmail.com>
2026-01-11 00:24:33 -0500

    tts initial

--- speech.py
+++ speech.py
@@ -3,10 +3,9 @@ import pyttsx3
 engine = pyttsx3.init()
 
 def say_interaction(text):
-    print(f"SocialSight says: {text}")
+    print(f"Optxt says: {text}")
     engine.say(text)
     engine.runAndWait()
 
-# Test it (this only runs if you run this file directly)
 if __name__ == "__main__":
-    say_interaction("Voice system is online.")
+    say_interaction("happy happy happy happy happy")
